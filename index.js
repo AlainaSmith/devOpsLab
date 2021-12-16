7885051c06a6b129fc48074743d55a8cf24ae202
Alaina <alaismit1@gmail.com>
2021-12-16 13:48:38 -0700

    third commit

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const path = require('path')
 
 
 
-
 const app = express()
 
 
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+
+
+
 const port = process.env.PORT || 4545
-
-
 
 app.listen(port, () => console.log(`Take us to warp ${port}!`))

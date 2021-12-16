970181c04b08d9bc7b4e5eb62ea60b9f32a83c8b
Alaina <alaismit1@gmail.com>
2021-12-16 16:53:13 -0700

    d

--- index.js
+++ index.js
@@ -23,6 +23,8 @@ const app = express()
 //     console.error(error);
 
 
+
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
     rollbar.info('html file served successfully.')
@@ -44,7 +46,22 @@ app.post('/api/student', () => (req, res) => {
 
     rollbar.log('Student added succesfully', {author:'Alaina', type: 'manual'})    //create students array up top
     res.status(200).send(students)
-})
+
+      const index = students.findIndex(studentName=> studentName === name)
+    
+        if(index === -1 && name !== ''){
+            students.push(name)
+            rollbar.log('Student added successfully', {author: 'Scott', type: 'manual entry'})
+            res.status(200).send(students)
+        } else if (name === ''){
+            rollbar.error('No name given')
+            res.status(400).send('must provide a name.')
+        } else {
+            rollbar.error('student already exists')
+            res.status(400).send('that student already exists')
+        }
+    
+    })
 
 app.use(rollbar.errorHandler())
 // rollbar.

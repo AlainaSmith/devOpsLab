02aaf47b773811f0f74ac219e106570a0f72a82f
Alaina <alaismit1@gmail.com>
2021-12-16 14:17:38 -0700

    fifth commit

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require('express')
 const path = require('path')
 
 const Rollbar = require('rollbar')
+
+
 const rollbar = new Rollbar({
   accessToken: '1f25d88fbfc74df88243ed100f4c2e71',
   captureUncaught: true,
   captureUnhandledRejections: true,
 })
+
+
 
 const app = express()
 

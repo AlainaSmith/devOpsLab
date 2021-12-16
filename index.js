6c4ce30bc333abde899efd84bc84e5d57450c155
Alaina <alaismit1@gmail.com>
2021-12-16 14:00:39 -0700

    fourth com

--- index.js
+++ index.js
@@ -3,15 +3,12 @@ const path = require('path')
 
 
 
+
 const app = express()
 
 
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+const port = process.env.PORT || 4545
 
 
-
-const port = process.env.PORT || 4646
 
 app.listen(port, () => console.log(`Take us to warp ${port}!`))
